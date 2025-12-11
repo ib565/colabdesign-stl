63c163ff48ea6eca0f88538aaaedcffbdc420a7a
ib565 <ish.bhartiya@gmail.com>
2025-12-12 01:44:40 +0530

    change con loss weight to 0.7

--- src/stl_designer.py
+++ src/stl_designer.py
@@ -140,7 +140,7 @@ class STLProteinDesigner:
         weights.update(
             {
                 "chamfer": chamfer_weight,
-                "con": weights.get("con", 1.0),
+                "con": weights.get("con", 0.7),
                 "i_con": weights.get("i_con", 0.0),
                 "plddt": plddt_weight,
                 "pae": pae_weight,
